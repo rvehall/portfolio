455444203c4307be99bf095710585fbf0079dba4
rvehall <raneith.dev@gmail.com>
2020-09-23 16:04:35 -0700

    deploy

---
+++ precache-manifest.79d478a977ab8bac91229c70366c3c47.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3342c206faadc0788ff",
+    "url": "/portfolio/css/chunk-vendors.6c27d868.css"
+  },
+  {
+    "revision": "84924d266f4f4f55b62f6b697c319d18",
+    "url": "/portfolio/favicon.jpg"
+  },
+  {
+    "revision": "73420a2502cc3afde7c9cc3c0218ef6f",
+    "url": "/portfolio/img/blog.73420a25.png"
+  },
+  {
+    "revision": "c73dd23b6a84d2901d748c9ed2294b5e",
+    "url": "/portfolio/img/isorry.c73dd23b.png"
+  },
+  {
+    "revision": "84924d266f4f4f55b62f6b697c319d18",
+    "url": "/portfolio/img/logo.84924d26.jpg"
+  },
+  {
+    "revision": "e7d8581b8303516b6f461a6f4961c3a8",
+    "url": "/portfolio/img/twj.e7d8581b.png"
+  },
+  {
+    "revision": "0c140d0b425ce8f06e3dc333b2127c6b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e89ede9a9e23a35a8033",
+    "url": "/portfolio/js/app.b81689ec.js"
+  },
+  {
+    "revision": "b3342c206faadc0788ff",
+    "url": "/portfolio/js/chunk-vendors.d4f6b047.js"
+  },
+  {
+    "revision": "6df918cb574d4975fe50df9708cdfdec",
+    "url": "/portfolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  }
+]);
